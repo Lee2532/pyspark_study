4561f367e9831b60416fa40f22fc8110ce5e6dd0
Lee2532 <seungoh0423@gmail.com>
2023-09-25 20:52:45 +0900

    pre-commit 테스트 결과

--- code/sample.py
+++ code/sample.py
@@ -7,7 +7,7 @@ from pyspark import SparkConf
 myconf = SparkConf()
 spark = pyspark.sql.SparkSession.builder.getOrCreate()
 
-test = [("1","kim","1993"),("2","park","1995")]
+test = [("1", "kim", "1993"), ("2", "park", "1995")]
 
 rdd = spark.sparkContext.parallelize(test)
 

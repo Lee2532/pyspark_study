b13f83d3b5312944323f47c6cffac83dc3428211
Lee2532 <seungoh0423@gmail.com>
2023-09-27 23:36:56 +0900

    스키마 및 테이블 생성 SQL

---
+++ setup/sqls/create.sql
@@ -0,0 +1,11 @@
+create schema broadcast;
+
+create table twitch
+(
+    user_id  text null,
+    nickname text null,
+    message  text  null,
+    channel  text  null,
+    create_time  text  null
+);
+
